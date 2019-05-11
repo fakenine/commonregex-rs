3d81a1a34889b189c3a6cbbd1f3f00fb601b2c77
Samy Kacimi <samy.kacimi@icloud.com>
2019-05-11 19:58:42 +0200

    add US phones

--- src/commonregex/phone.rs
+++ src/commonregex/phone.rs
@@ -1,6 +1,7 @@
 use super::matcher;
 
 const FR_REGEX: &'static str = r#"(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}"#;
+const US_REGEX: &'static str = r#"(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}"#;
 
 /// Returns matched French phone numbers as a vector of strings
 ///
@@ -31,6 +32,27 @@ pub fn fr(text: &String) -> Vec<&str> {
     matcher::match_results(text, FR_REGEX)
 }
 
+/// Returns matched US phone numbers as a vector of strings
+///
+/// # Arguments
+///
+/// * `text` - A String representing the text in which to search.
+///
+/// # Examples
+///
+/// ```
+/// extern crate commonregex_rs;
+///
+/// use commonregex_rs::commonregex;
+///
+/// let text = String::from("Let's go to the coffee! Here's my number 202-555-0104");
+///
+/// assert_eq!(vec!["202-555-0104"], commonregex::phone::us(&text));
+/// ```
+pub fn us(text: &String) -> Vec<&str> {
+    matcher::match_results(text, US_REGEX)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -51,5 +73,11 @@ mod tests {
     fn phone_fr_dots() {
         let text = String::from("On se rejoint au café ? Voici mon num 06.06.06.06.06");
         assert_eq!(vec!["06.06.06.06.06"], fr(&text));
+    }
+
+    #[test]
+    fn phone_us() {
+        let text = String::from("Let's go to the coffee! Here's my number 202-555-0104");
+        assert_eq!(vec!["202-555-0104"], us(&text));
     }
 }
