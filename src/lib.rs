49a09a8c1db97617e8156f24b7f01ab412e466a7
Samy Kacimi <samy.kacimi@icloud.com>
2019-05-11 13:38:53 +0200

    use String as argument for text

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod commonregex {
     use regex::Regex;
     use super::regex_drawer;
 
-    fn match_results<'a>(text: &'a str, regex: &'static str) -> Vec<&'a str> {
+    fn match_results<'a>(text: &'a String, regex: &'static str) -> Vec<&'a str> {
         let re = Regex::new(regex).unwrap();
 
         re.find_iter(text).map(|mat| mat.as_str()).collect()
@@ -15,7 +15,7 @@ pub mod commonregex {
     pub mod ip {
         use super::*;
 
-        pub fn v4(text: &str) -> Vec<&str> {
+        pub fn v4(text: &String) -> Vec<&str> {
             match_results(text, regex_drawer::ip::V4)
         }
     }
@@ -27,7 +27,7 @@ mod tests {
 
     #[test]
     fn ipv4() {
-        let text = "Started GET '/' for 127.0.0.1 at 2019-05-11 00:51:35";
-        assert_eq!(vec!["127.0.0.1"], commonregex::ip::v4(text));
+        let text = String::from("Started GET '/' for 127.0.0.1 at 2019-05-11 00:51:35");
+        assert_eq!(vec!["127.0.0.1"], commonregex::ip::v4(&text));
     }
 }
