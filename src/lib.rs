cabfbd92c3ea7c021ec65088e7a7df254bc03393
Samy Kacimi <samy.kacimi@icloud.com>
2019-05-11 13:14:26 +0200

    move regexes to another module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 extern crate regex;
 
+mod regex_drawer;
+
 pub mod commonregex {
     use regex::Regex;
-
-    const IPV4: &'static str = r"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+    use super::regex_drawer;
 
     fn match_results<'a>(text: &'a str, regex: &'static str) -> Vec<&'a str> {
         let re = Regex::new(regex).unwrap();
@@ -11,8 +12,12 @@ pub mod commonregex {
         re.find_iter(text).map(|mat| mat.as_str()).collect()
     }
 
-    pub fn ipv4(text: &str) -> Vec<&str> {
-        match_results(text, IPV4)
+    pub mod ip {
+        use super::*;
+
+        pub fn v4(text: &str) -> Vec<&str> {
+            match_results(text, regex_drawer::ip::V4)
+        }
     }
 }
 
@@ -21,8 +26,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ipv4_ok() {
+    fn ipv4() {
         let text = "Started GET '/' for 127.0.0.1 at 2019-05-11 00:51:35";
-        assert_eq!(vec!["127.0.0.1"], commonregex::ipv4(text));
+        assert_eq!(vec!["127.0.0.1"], commonregex::ip::v4(text));
     }
 }
