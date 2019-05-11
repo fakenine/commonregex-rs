7c931b1cd94e05a8db7987c7cd87f6521cdb0daa
Samy Kacimi <samy.kacimi@icloud.com>
2019-05-11 13:39:30 +0200

    add documentation for ipv4

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,23 @@ pub mod commonregex {
     pub mod ip {
         use super::*;
 
+        /// Returns matched IPv4 addresses as a vector of strings
+        ///
+        /// # Arguments
+        ///
+        /// * `text` - A String representing the text in which to search.
+        ///
+        /// # Examples
+        ///
+        /// ```
+        /// extern crate commonregex_rs;
+        ///
+        /// use commonregex_rs::commonregex;
+        ///
+        /// let log = String::from("Started GET '/' for 127.0.0.1 at 2019-05-11 00:51:35");
+        ///
+        /// assert_eq!(vec!["127.0.0.1"], commonregex::ip::v4(&log));
+        /// ```
         pub fn v4(text: &String) -> Vec<&str> {
             match_results(text, regex_drawer::ip::V4)
         }
